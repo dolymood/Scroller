ff65ac5f292fdab5e9770f51dbdee94eb8c074a0
dolymood <dolymood@gmail.com>
2013-07-18 21:18:22 +0800

    c

--- scroller.js
+++ scroller.js
@@ -85,7 +85,7 @@
         });
         return obj;
     }
-    var BACK_CONST = 1.70158;
+
     Scroller.easing = {
 
         linear : function(t){
@@ -100,84 +100,8 @@
             return ( 2 - t) * t;
         },
 
-        easeBoth : function(t){
-            return (t *= 2) < 1 ?
-                .5 * t * t :
-                .5 * (1 - (--t) * (t - 2));
-        },
-
-        easeInStrong : function(t){
-            return t * t * t * t;
-        },
-
-        easeOutStrong : function(t){
-            return 1 - (--t) * t * t * t;
-        },
-
-        easeBothStrong : function(t){
-            return (t *= 2) < 1 ?
-                .5 * t * t * t * t :
-                .5 * (2 - (t -= 2) * t * t * t);
-        },
-        
-        easeOutQuart : function(t){
-          return -(Mth.pow((t-1), 4) -1)
-        },
-        
-        easeInOutExpo : function(t){
-            if(t===0) return 0;
-            if(t===1) return 1;
-            if((t/=0.5) < 1) return 0.5 * Mth.pow(2,10 * (t-1));
-            return 0.5 * (-Mth.pow(2, -10 * --t) + 2);
-        },
-        
-        easeOutExpo : function(t){
-            return (t===1) ? 1 : -Mth.pow(2, -10 * t) + 1;
-        },
-        
         swing : function( t ) {
             return 0.5 - Mth.cos(t * Mth.PI) / 2;
-        },
-        
-        swingFrom : function(t){
-            return t*t*((BACK_CONST+1)*t - BACK_CONST);
-        },
-        
-        swingTo : function(t){
-            return (t-=1)*t*((BACK_CONST+1)*t + BACK_CONST) + 1;
-        },
-
-        backIn : function(t){
-            if (t === 1) t -= .001;
-            return t * t * ((BACK_CONST + 1) * t - BACK_CONST);
-        },
-
-        backOut : function(t){
-            return (t -= 1) * t * ((BACK_CONST + 1) * t + BACK_CONST) + 1;
-        },
-
-        bounce : function(t){
-            var s = 7.5625, r;
-
-            if (t < (1 / 2.75)) {
-                r = s * t * t;
-            }
-            else if (t < (2 / 2.75)) {
-                r = s * (t -= (1.5 / 2.75)) * t + .75;
-            }
-            else if (t < (2.5 / 2.75)) {
-                r = s * (t -= (2.25 / 2.75)) * t + .9375;
-            }
-            else {
-                r = s * (t -= (2.625 / 2.75)) * t + .984375;
-            }
-
-            return r;
-        },
-        
-        // windows8开始面板的滑动切换效果
-        doubleSqrt : function( t ){
-            return Mth.sqrt(Mth.sqrt(t));
         }
 
     };
